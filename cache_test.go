c932c1c3bb9a9f301016df490d7323c43c03d2ab
Matt Evans <mattevansnz@gmail.com>
2019-06-12 05:48:29 +1000

    Updated tests

--- cache_test.go
+++ cache_test.go
@@ -19,20 +19,25 @@ func TestCache(t *testing.T) {
 	// Get latest forex rates.
 	response1, err := client.Rates.List()
 	if err != nil {
-		t.Fatalf("Unexpected error running client.Rates.All(): %s", err.Error())
+		t.Fatalf("Unexpected error running client.Rates.List(): %s", err.Error())
 	}
+
+	// Fetch results again
+	response2, ok := client.Cache.Get("AUD")
+	if !ok {
+		t.Fatalf("Expected response when fetching from cache for base currency AUD, got: %v", response2)
+	}
+
+	first, _ := json.Marshal(response1)
+	second, _ := json.Marshal(response2)
+	Expect(first).To(MatchJSON(second))
 
 	// Expire the cache
 	client.Cache.Expire("AUD")
 
-	// Fetch results again
-	response2, err := client.Rates.List()
-	if err != nil {
-		t.Fatalf("Unexpected error running client.Rates.All(): %s", err.Error())
-	}
+	// Fetch results again (from the cache), now it's cleared.
+	response2, _ = client.Cache.Get("AUD")
 
-	// Compare the results, they shouldn't match, as timestamp values will differ.
-	first, _ := json.Marshal(response1)
-	second, _ := json.Marshal(response2)
-	Expect(first).NotTo(MatchJSON(second))
+	// Should be nothing.
+	Expect(response2).Should(BeNil())
 }
